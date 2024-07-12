b095625db845cf0baa499b15e2c088a1a800e00d
OnkarBorude <onkarb749@gmail.com>
2024-07-12 13:18:54 +0530

    API_LOGIN_ADDUSER_DTO_DBModified

--- ecommerce-api/src/main/java/org/dnyanyog/service/AddUserService.java
+++ ecommerce-api/src/main/java/org/dnyanyog/service/AddUserService.java
@@ -1,5 +1,6 @@
 package org.dnyanyog.service;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.dnyanyog.common.DBUtils;
@@ -12,16 +13,32 @@ public class AddUserService {
 	public AddUserResponse addUser(AddUserRequest addUserRequest) throws SQLException {
 		AddUserResponse addUserResponse=new AddUserResponse();
 		String insertQuery="insert into user(first_name,last_name,gender,age,email,password) values('"+addUserRequest.first_name+"','"+addUserRequest.last_name+"','"+addUserRequest.gender+"','"+addUserRequest.age+"','"+addUserRequest.email+"','"+addUserRequest.password+"')";
-		int rowaffected =DBUtils.executeQuery(insertQuery);
-		if(rowaffected>0) {
+		DBUtils.executeQuery(insertQuery);
+		String selectQuery="select * from user where first_name='"+addUserRequest.first_name+"'";
+		ResultSet resultSet=DBUtils.selectQuery(selectQuery);
+		if(resultSet.next()) {
 			addUserResponse.errorCode="0000";
 			addUserResponse.messege="Add User Succesfully";
-			return addUserResponse;
-		}else {
+		return addUserResponse;
+			
+		}
+		else {
 			addUserResponse.errorCode="911";
 			addUserResponse.messege="User Additon Failed";
-			return addUserResponse;
+		return addUserResponse;
 		}
+		
+		
+//		int rowaffected =DBUtils.executeQuery(insertQuery);
+//		if(rowaffected>0) {
+//			addUserResponse.errorCode="0000";
+//			addUserResponse.messege="Add User Succesfully";
+//			return addUserResponse;
+//		}else {
+//			addUserResponse.errorCode="911";
+//			addUserResponse.messege="User Additon Failed";
+//			return addUserResponse;
+//		}
 	}
 
 }
